4f18796aac9c8e46227670e6183c28ebb5ad2272
Colin Willits <24CWillits@wpss.org>
2023-12-08 09:15:35 -0500

    Second

--- subsystems/intakesubsystem.py
+++ subsystems/intakesubsystem.py
@@ -9,9 +9,9 @@ import constants
 
 class IntakeSubsystem(SubsystemBase):
     class Mode(Enum):
-        Deployed = auto()
-        Retracted = auto()
-        Reversed = auto()
+        Dep = auto()
+        Ret = auto()
+        Rev = auto()
 
     def __init__(self):
         SubsystemBase.__init__(self)
@@ -36,4 +36,33 @@ class IntakeSubsystem(SubsystemBase):
             ),
         ):
             return
+        if not ctreCheckError(
+            "config_kI",
+            self.intakeMotor.config_kI(
+                constants.kIntakePIDSlot,
+                constants.kIntakeIGain,
+                constants.kConfigurationTimeoutLimit,
+            ),
+        ):
+            return
+        if not ctreCheckError(
+            "config_kD",
+            self.intakeMotor.config_kD(
+                constants.kIntakePIDSlot,
+                constants.kIntakeDGain,
+                constants.kConfigurationTimeoutLimit,
+            ),
+        ):
+            return
+        print(f"{constants.kIntakeMotorName} Initialization Complete")
+        self.state = self.Mode.Ret
+
+    def period(self) -> None:
+        if self.state == self.Mode.Dep:
+            self.intakeMotor.set(
+                ControlMode.Velocity,
+                constants.kIntakeSpeed
+                * constants.kIntakeGearRatio
+                * constants.kTalonVelocityPerRPM,
+            )
 
